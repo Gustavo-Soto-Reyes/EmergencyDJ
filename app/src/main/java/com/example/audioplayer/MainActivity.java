7ef72119658ca552384ed32d9ca3d86a5362df81
Gustavo-Soto-Reyes <gsotoreyes73@gmail.com>
2023-12-07 17:19:19 -0800

    fix crash | reuse MediaPlayers | add more tracks and skip ability

--- app/src/main/java/com/example/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -7,75 +7,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    Button button1, button2, button3, button4, button5, button6, button7 ;
-    int[] samples = {R.raw.music, R.raw.dance, R.raw.dance2};
+    Button button1;
+    private int[] musicResources = {R.raw.music, R.raw.dance, R.raw.dance2};
+    private Button[] buttons;
+    private int[] soundResources = {
+            R.raw.horn,
+            R.raw.beep, R.raw.explosion, R.raw.scream,
+            R.raw.siren, R.raw.wow
+    };
+    private MediaPlayer[] soundPlayers;
+    private MediaPlayer audioPlayer;
+
     int currentTrack = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttons = new Button[]{
+                findViewById(R.id.button2),
+                findViewById(R.id.button3), findViewById(R.id.button4),
+                findViewById(R.id.button5), findViewById(R.id.button6),
+                findViewById(R.id.button7)
+        };
+
+        soundPlayers = new MediaPlayer[buttons.length];
+
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setOnClickListener(view -> {
+                int buttonIndex = getButtonIndex(view);
+                playSound(soundResources[buttonIndex], buttonIndex);
+            });
+        }
         button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-        button5 = findViewById(R.id.button5);
-        button6 = findViewById(R.id.button6);
-        button7 = findViewById(R.id.button7);
-
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-        button6.setOnClickListener(this);
-        button7.setOnClickListener(this);
-
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PlaySounds(samples[currentTrack]);
-                currentTrack = (currentTrack+1)%3;
+        button1.setOnClickListener(view -> {
+            if (audioPlayer != null){
+                audioPlayer.release();
             }
+            audioPlayer = MediaPlayer.create(getApplicationContext(), musicResources[currentTrack]);
+            audioPlayer.start();
+
+            currentTrack = (currentTrack+1)%3;
         });
+    }
+    private int getButtonIndex(View view) {
+        for (int i = 0; i < buttons.length; i++) {
+            if (view.getId() == buttons[i].getId()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    private void playSound(int soundResource, int mediaPlayerIndex) {
+        stopMedia(mediaPlayerIndex);
 
+        soundPlayers[mediaPlayerIndex] = MediaPlayer.create(this, soundResource);
+        soundPlayers[mediaPlayerIndex].start();
     }
 
-    @Override
-    public void onClick(View view) {
-        int clickedButton = view.getId();
-        if (clickedButton == R.id.button1){
-            PlaySounds(R.raw.music);
+    private void stopMedia(int mediaPlayerIndex) {
+        if (soundPlayers[mediaPlayerIndex] != null) {
+            soundPlayers[mediaPlayerIndex].stop();
+            soundPlayers[mediaPlayerIndex].release();
+            soundPlayers[mediaPlayerIndex] = null;
         }
-        if (clickedButton == R.id.button2){
-            PlaySounds(R.raw.horn);
-        }
-
-        if (clickedButton == R.id.button3){
-            PlaySounds(R.raw.beep);
-        }
-        if (clickedButton == R.id.button4){
-            PlaySounds(R.raw.explosion);
-        }
-
-        if (clickedButton == R.id.button5){
-            PlaySounds(R.raw.scream);
-        }
-
-        if (clickedButton == R.id.button6){
-            PlaySounds(R.raw.siren);
-        }
-
-        if (clickedButton == R.id.button7){
-            PlaySounds(R.raw.wow);
-        }
-    }
-
-    public void PlaySounds(int id){
-        MediaPlayer mediaPlayer =  MediaPlayer.create(this, id);
-        mediaPlayer.start();
     }
 }
